0b76561cb76d766bffabb44926213e56c52318d6
FaerGun <57860661+FaerGun@users.noreply.github.com>
2023-03-27 19:35:40 +0300

    Add files via upload

---
+++ tetris/tetris/DrawText.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+void draw_text(unsigned short i_x, unsigned short i_y, const std::string& i_text, class sf::RenderWindow& i_window);
+
---
+++ tetris/tetris/GetTetromino.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include "Global.hpp"
+
+std::vector<Position> get_tetromino(unsigned char i_shape, unsigned char i_x, unsigned char i_y);
